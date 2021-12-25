6ee031ca51d37bc3dccbff63ddd2a3f3a6a19472
DanhDrako <nkocdanh58@gmail.com>
2021-12-25 18:15:01 +0700

    update file

--- helloworld.js
+++ helloworld.js
@@ -1,6 +1,6 @@
 const strings =['hello']
-const string1= ['so']
+//const string1= ['so']
 strings.push('world') //hello ,world, a
-string1.push('123')
+//string1.push('123')
 console.log(strings)
-console.log(string1)
+//console.log(string1)
