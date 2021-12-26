e65fb3d6dc1701cc666fef6f5a2f731f1e021f53
Danh Drako <92949082+DanhDrako@users.noreply.github.com>
2021-12-26 16:29:49 +0700

    Update helloworld.js

--- helloworld.js
+++ helloworld.js
@@ -1,3 +1,6 @@
 const strings =['hello']
-strings.push('world') //hello ,world, a
+strings.push('world') //hello ,world
 console.log(strings)
+const string1 =['so']
+string1.push('123') //
+console.log(string1)
